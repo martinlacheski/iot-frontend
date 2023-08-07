9be9fe6464c73ab7c1b1a74ded498847916b53dd
mlacheski <martinlacheski@fceqyn.unam.edu.ar>
2023-08-07 14:27:40 -0300

    front en tipos de equipamientos y equipamientos - refs #18
    Co-Authored-By: Matías Agustín Benítez <109183015+matiasagbenitez@users.noreply.github.com>

--- src/iot/pages/parameters/EquipmentTypes.jsx
+++ src/iot/pages/parameters/EquipmentTypes.jsx
@@ -1,17 +1,299 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
+import iotApi from "../../../api/iotApi";
+import { DataGrid } from "@mui/x-data-grid";
+import {
+  Box,
+  Button,
+  Divider,
+  Modal,
+  TextField,
+  Grid,
+  Typography,
+  Alert,
+} from "@mui/material";
+import {
+  ModeEditOutlineOutlined,
+  DeleteOutlineOutlined,
+} from "@mui/icons-material";
+import { useForm } from "../../../hooks/useForm";
+import { FlexBetween } from "../../components/FlexBetween";
+import {
+  showSuccessToast,
+  showErrorAlert,
+  showConfirmationAlert,
+} from "../../../utils";
+
+const formData = {
+  name: "",
+};
+
+const formValidations = {
+  name: [(value) => value.trim() !== "", "El nombre es obligatorio."],
+};
 
 export const EquipmentTypes = () => {
+  const [equipmentTypes, setEquipmentTypes] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [editEquipmentId, setEditEquipmentId] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const {
+    formState,
+    name,
+    onInputChange,
+    isFormValid,
+    nameValid,
+    onResetForm,
+  } = useForm(formData, formValidations);
+
+  useEffect(() => {
+    fetchEquipmentTypes();
+  }, []);
+
+  // OBTENER TIPOS DE EQUIPOAMIENTO
+  const fetchEquipmentTypes = async () => {
+    try {
+      const { data } = await iotApi.get("/types-of-equipments");
+      setEquipmentTypes(data.typeOfEquipments);
+    } catch (error) {
+      console.log("Error fetching typeOfEquipments:", error);
+    }
+  };
+
+  // CREAR TIPO DE EQUIPOAMIENTO
+  const handleCreateEquipmentType = async () => {
+    setFormSubmitted(true);
+    if (!isFormValid) return;
+    try {
+      const { data } = await iotApi.post("/types-of-equipments", formState);
+      setEquipmentTypes([...equipmentTypes, data.typeOfEquipment]);
+      handleCloseModal();
+      showSuccessToast(data.msg);
+    } catch (error) {
+      showErrorAlert(error.response.data?.msg);
+    }
+  };
+
+  // EDITAR TIPO DE EQUIPOAMIENTO
+  const handleEditEquipmentType = async (id) => {
+    const equipmentTypeToEdit = equipmentTypes.find((type) => type._id === id);
+    if (equipmentTypeToEdit) {
+      onInputChange({
+        target: {
+          name: "name",
+          value: equipmentTypeToEdit.name,
+        },
+      });
+      setEditEquipmentId(id);
+      handleOpenModal();
+    }
+  };
+
+  const updateTypeOfEquipment = async () => {
+    setFormSubmitted(true);
+    if (!isFormValid) return;
+    try {
+      const { data } = await iotApi.put(
+        `/types-of-equipments/${editEquipmentId}`,
+        formState
+      );
+      setEquipmentTypes(
+        equipmentTypes.map((type) =>
+          type._id === editEquipmentId ? data.typeOfEquipment : type
+        )
+      );
+      handleCloseModal();
+      showSuccessToast(data.msg);
+    } catch (error) {
+      showErrorAlert(error.response.data?.msg);
+    }
+  };
+
+  // ELIMINAR TIPO DE EQUIPOAMIENTO
+  const handleDelete = async (id) => {
+    try {
+      const confirmation = await showConfirmationAlert(
+        "¿Confirmas la eliminación del tipo de equipamiento?"
+      );
+      if (confirmation.isConfirmed) {
+        const { data } = await iotApi.delete(`/types-of-equipments/${id}`);
+        setEquipmentTypes(equipmentTypes.filter((type) => type._id !== id));
+        showSuccessToast(data.msg);
+      }
+    } catch (error) {
+      showErrorAlert(error.response.data?.msg);
+    }
+  };
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setEditEquipmentId(null);
+    setFormSubmitted(false);
+    setErrorMessage(null);
+    onResetForm();
+  };
+
+  const onSubmit = () => {
+    if (editEquipmentId) {
+      updateTypeOfEquipment();
+    } else {
+      handleCreateEquipmentType();
+    }
+  };
+
+  const columns = [
+    { field: "_id", headerName: "ID", flex: 0.5 },
+    { field: "name", headerName: "Nombre", flex: 0.5 },
+    {
+      field: "actions",
+      headerName: "Acciones",
+      flex: 0.5,
+      renderCell: (params) => (
+        <Box
+          sx={{
+            display: "flex",
+            gap: "1rem",
+          }}
+        >
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={() => handleEditEquipmentType(params.row._id)}
+          >
+            Editar
+          </Button>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={() => handleDelete(params.row._id)}
+          >
+            Eliminar
+          </Button>
+        </Box>
+      ),
+    },
+  ];
+
+  // Función para obtener el ID de una fila
+  const getRowId = (row) => row._id;
+
   return (
     <Fragment>
-
       <Header
-        title="Tipos de equipamiento"
-        subtitle="En esta sección podrá administrar los distintos tipos de equipamiento del sistema."
+        title="Tipos de equipamientos"
+        subtitle="En esta sección podrá administrar los tipos de equipamientos del sistema."
       />
 
-      {/* CUERPO DE LA PÁGINA */}
+      <Box
+        sx={{
+          height: "60vh",
+          width: "100%",
+          padding: "1rem 2rem",
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: "#e5e7eb",
+            fontWeight: "bold",
+            textTransform: "uppercase",
+            borderBottom: "none",
+          },
+        }}
+      >
+        <FlexBetween
+          sx={{
+            alignContent: "center",
+            mb: "1rem",
+          }}
+        >
+          <Typography sx={{ mb: "1rem", fontWeight: "bold", m: "0" }}>
+            Listado de tipos de equipamientos
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleOpenModal}
+            sx={{ p: ".3rem 1rem" }}
+          >
+            Crear tipo de equipamiento
+          </Button>
+        </FlexBetween>
+        <DataGrid
+          loading={!equipmentTypes}
+          rows={equipmentTypes}
+          columns={columns}
+          getRowId={getRowId}
+        />
+      </Box>
 
+      <Modal open={isModalOpen} onClose={handleCloseModal}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: "1.5rem",
+            minWidth: 400,
+            borderRadius: "1rem",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: 18,
+              textTransform: "uppercase",
+              mb: "1rem",
+              fontWeight: "bold",
+            }}
+          >
+            {editEquipmentId ? "Editar tipo de equipamiento" : "Crear nuevo tipo de equipamiento"}
+          </Typography>
+          <Divider sx={{ mb: "1rem" }} />
+
+          <Grid container gap={1}>
+            <Grid item xs={12}>
+              <TextField
+                label="Nombre del tipo de equipamiento"
+                name="name"
+                value={name}
+                onChange={onInputChange}
+                fullWidth
+                error={!!nameValid && formSubmitted}
+                helperText={!!nameValid && formSubmitted ? nameValid : ""}
+              />
+            </Grid>
+            <Grid item xs={12} display={!!errorMessage ? "" : "none"}>
+              <Alert severity="error">
+                <Typography variant="body2">{errorMessage}</Typography>
+              </Alert>
+            </Grid>
+          </Grid>
+         <Box sx={{float: "right"}}>
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={handleCloseModal}
+              style={{ marginTop: "1rem", marginRight: ".5rem"}}
+            >
+              Cancelar
+            </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={onSubmit}
+              style={{ marginTop: "1rem"}}
+              disabled={!isFormValid}
+            >
+              {editEquipmentId ? "Guardar" : "Crear"}
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
     </Fragment>
   );
 };
